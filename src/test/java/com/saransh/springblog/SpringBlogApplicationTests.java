2e09da86f4a71bfc560efe7bcd348411e104576b
Saransh Kumar <cryptosingh@gmail.com>
2021-07-27 13:57:24 +0530

    Closes #1: Initialize Spring Project.

---
+++ src/test/java/com/saransh/springblog/SpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saransh.springblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
