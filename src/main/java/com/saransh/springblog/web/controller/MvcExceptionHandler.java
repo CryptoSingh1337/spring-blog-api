e255fee625325929ea3aa4f328034894032fd3c9
Saransh Kumar <cryptosingh@gmail.com>
2021-07-28 21:09:12 +0530

    Closes #20: Create Global Exception Handler.

---
+++ src/main/java/com/saransh/springblog/web/controller/MvcExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.saransh.springblog.web.controller;
+
+import com.saransh.springblog.Exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * Created by CryptoSingh1337 on 7/28/2021
+ */
+@ControllerAdvice
+public class MvcExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> resourceNotFoundHandler(ResourceNotFoundException e) {
+        return new ResponseEntity<>(
+                e.getMessage(),
+                HttpStatus.NOT_FOUND
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> validationHandler(MethodArgumentNotValidException e) {
+        return ResponseEntity.badRequest()
+                .body(
+                        e.getAllErrors()
+                );
+    }
+}
