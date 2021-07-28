bdaf41af16f8f0ea71cc24a5c765ce5cf250ea02
Saransh Kumar <cryptosingh@gmail.com>
2021-07-28 21:07:13 +0530

    Closes #16: Fix DateMapper and PostMapper.

--- src/main/java/com/saransh/springblog/web/mapper/DateMapper.java
+++ src/main/java/com/saransh/springblog/web/mapper/DateMapper.java
@@ -1,6 +1,6 @@
 package com.saransh.springblog.web.mapper;
 
-import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
@@ -9,13 +9,17 @@ import java.time.ZoneOffset;
 /**
  * Created by CryptoSingh1337 on 7/27/2021
  */
-@Mapper
+@Component
 public class DateMapper {
     public OffsetDateTime asOffsetDateTime(LocalDateTime ld) {
-        return OffsetDateTime.of(ld, ZoneOffset.UTC);
+        if (ld != null)
+            return OffsetDateTime.of(ld, ZoneOffset.UTC);
+        return null;
     }
 
     public LocalDateTime asLocalDateTime(OffsetDateTime of) {
-        return of.toLocalDateTime();
+        if (of != null)
+            return of.toLocalDateTime();
+        return null;
     }
 }
--- src/main/java/com/saransh/springblog/web/mapper/PostMapper.java
+++ src/main/java/com/saransh/springblog/web/mapper/PostMapper.java
@@ -3,7 +3,6 @@ package com.saransh.springblog.web.mapper;
 import com.saransh.springblog.domain.Post;
 import com.saransh.springblog.web.model.PostDTO;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 /**
  * Created by CryptoSingh1337 on 7/27/2021
@@ -11,9 +10,6 @@ import org.mapstruct.Mapping;
 @Mapper(uses = {DateMapper.class})
 public interface PostMapper {
 
-    @Mapping(target = "updatedAt", ignore = true)
     Post postDTOToPost(PostDTO postDTO);
-
-    @Mapping(target = "updatedAt", ignore = true)
     PostDTO postToPostDTO(Post post);
 }
