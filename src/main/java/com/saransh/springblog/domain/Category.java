1722090b6924c7a8578e4220873a02c78d9dee4a
Saransh Kumar <cryptosingh@gmail.com>
2021-07-30 13:01:07 +0530

    Closes #35: Fix ID value in Comment Table is not starting from 1.

--- src/main/java/com/saransh/springblog/domain/Category.java
+++ src/main/java/com/saransh/springblog/domain/Category.java
@@ -21,7 +21,7 @@ import javax.persistence.Id;
 public class Category {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
 }
--- src/main/java/com/saransh/springblog/domain/Comment.java
+++ src/main/java/com/saransh/springblog/domain/Comment.java
@@ -17,7 +17,7 @@ import java.time.LocalDateTime;
 public class Comment {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     @Lob
